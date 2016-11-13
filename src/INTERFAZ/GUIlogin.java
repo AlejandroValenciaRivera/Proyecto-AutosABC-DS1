629ba58be589cb904dbbacedeefe5a03f2d22625
AlejandroValenciaRivera <alejandrovr.144@gmail.com>
2016-11-13 13:51:41 -0500

    Modificación: GUIlogin-cambio en la conexion a la base de datos, nombre de base de datos erroneo

--- src/INTERFAZ/GUIlogin.java
+++ src/INTERFAZ/GUIlogin.java
@@ -154,7 +154,7 @@ public class GUIlogin extends javax.swing.JFrame {
         int cargo = 0;
 
         String driver = "org.postgresql.Driver";
-        String connectString = "jdbc:postgresql://localhost:5432/AutosABC";
+        String connectString = "jdbc:postgresql://localhost:5432/autosabc";
         String user = "postgres";
         String password = "123456789";
 
