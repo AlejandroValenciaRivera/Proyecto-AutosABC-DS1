fc3beed72124e7be3b7d50b34c6ac52f026bbfc8
julicampi <julian.campino@correounivalle.edu.co>
2016-11-14 19:32:52 -0500

    Update Vehiculo.java

--- src/ACCESO_DATOS/entidades_y_relaciones/Vehiculo.java
+++ src/ACCESO_DATOS/entidades_y_relaciones/Vehiculo.java
@@ -1,14 +1,104 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ACCESO_DATOS.entidades_y_relaciones;
 
+package ACCESO_DATOS.entidades_y_relaciones;
 /**
  *
  * @author aleja_000
  */
 public class Vehiculo {
+        int vehiculo ;
+	Sede id_sede ;
+	String color ;
+	String marca;
+	String referencia ;
+	String tipo ;
+	String traccion ;
+	String modelo ;
+	int precio ;
+	int iva ;
+
+    public int getVehiculo() {
+        return vehiculo;
+    }
+
+    public Sede getId_sede() {
+        return id_sede;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getTraccion() {
+        return traccion;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public int getIva() {
+        return iva;
+    }
+
+    public void setVehiculo(int vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+
+    public void setId_sede(Sede id_sede) {
+        this.id_sede = id_sede;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setTraccion(String traccion) {
+        this.traccion = traccion;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+
+
+    
+    
+    
     
 }
