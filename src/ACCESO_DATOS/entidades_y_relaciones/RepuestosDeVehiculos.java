300aa80eea73495fbc09ad39e6f713c3031bf05d
julicampi <julian.campino@correounivalle.edu.co>
2016-11-14 19:32:11 -0500

    Update RepuestosDeVehiculos.java

--- src/ACCESO_DATOS/entidades_y_relaciones/RepuestosDeVehiculos.java
+++ src/ACCESO_DATOS/entidades_y_relaciones/RepuestosDeVehiculos.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ACCESO_DATOS.entidades_y_relaciones;
 
 /**
@@ -11,4 +7,24 @@ package ACCESO_DATOS.entidades_y_relaciones;
  */
 public class RepuestosDeVehiculos {
     
-}
+        Repuesto id_repuesto ;
+	Vehiculo id_vehiculo ;
+
+    public Repuesto getId_repuesto() {
+        return id_repuesto;
+    }
+
+    public Vehiculo getId_vehiculo() {
+        return id_vehiculo;
+    }
+
+    public void setId_repuesto(Repuesto id_repuesto) {
+        this.id_repuesto = id_repuesto;
+    }
+
+    public void setId_vehiculo(Vehiculo id_vehiculo) {
+        this.id_vehiculo = id_vehiculo;
+    }
+
+        
+   }
