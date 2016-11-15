c4af96f8efe407102dcc38d0dba6208fbec7ba9e
julicampi <julian.campino@correounivalle.edu.co>
2016-11-14 19:31:13 -0500

    Update Repuesto.java

--- src/ACCESO_DATOS/entidades_y_relaciones/Repuesto.java
+++ src/ACCESO_DATOS/entidades_y_relaciones/Repuesto.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ACCESO_DATOS.entidades_y_relaciones;
 
 /**
@@ -10,5 +6,74 @@ package ACCESO_DATOS.entidades_y_relaciones;
  * @author aleja_000
  */
 public class Repuesto {
+    
+        int id_repuesto;
+	Sede id_sede ;
+	String nombre ;
+	int cantidad;
+	int precio_unidad ;
+	int iva;
+	String descripcion ;
+
+    public int getId_repuesto() {
+        return id_repuesto;
+    }
+
+    public Sede getId_sede() {
+        return id_sede;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public int getPrecio_unidad() {
+        return precio_unidad;
+    }
+
+    public int getIva() {
+        return iva;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setId_repuesto(int id_repuesto) {
+        this.id_repuesto = id_repuesto;
+    }
+
+    public void setId_sede(Sede id_sede) {
+        this.id_sede = id_sede;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void setPrecio_unidad(int precio_unidad) {
+        this.precio_unidad = precio_unidad;
+    }
+
+    public void setIva(int iva) {
+        this.iva = iva;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+	
+    
+       
+    
+    
     
 }
