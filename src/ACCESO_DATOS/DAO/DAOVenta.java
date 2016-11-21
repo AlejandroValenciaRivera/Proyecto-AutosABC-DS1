5dc0a8616c1a0a5c3cb5a614e3434678ff0d73ac
proyecto-ds1 <proyecto-ds1@proyectods1-VirtualBox>
2016-11-21 14:42:30 -0500

    JUJO: Se programó la consulta de venta

--- src/ACCESO_DATOS/DAO/DAOVenta.java
+++ src/ACCESO_DATOS/DAO/DAOVenta.java
@@ -8,6 +8,7 @@ package ACCESO_DATOS.DAO;
 import ACCESO_DATOS.conexion.*;
 import ACCESO_DATOS.entidades_y_relaciones.*;
 import java.sql.*;
+import javax.swing.JOptionPane;
 /**
  *
  * @author aleja_000
@@ -54,8 +55,48 @@ public class DAOVenta {
         return numFilas;
     
     }
-    
-    public Venta consultarVenta() {
-        return null;
+     /**
+     * 
+     * @param idVenta
+     * @return int
+     * @author Juan José Varela V
+     * @proposito recive un objeto Venta y lo guarda en la base de datos
+     */   
+    public Venta consultarVenta(int idVenta) {
+
+        Venta ven = new Venta();
+        String unRep = "SELECT id_venta, "
+                + "id_vehiculo,"
+                + "id_usuario,"
+                + "id_sede, "
+                + "formaDePago, "
+                + "fecha, "
+                + "cedula_cliente, "
+                + "nombre_cliente, "
+                + "telefono_cliente "
+                + "FROM repuestos WHERE id_repuesto = " + idVenta;
+        try{
+            Connection conn = fachada.getConnetion();
+            Statement sentencia = conn.createStatement();
+            ResultSet tabla = sentencia.executeQuery(unRep);
+            
+            ven.setId_venta(tabla.getInt(1));
+            ven.setId_vehiculo(tabla.getInt(2));
+            ven.setId_usuario(tabla.getInt(3));
+            ven.setId_sede(tabla.getInt(4));
+            ven.setForma_pago(tabla.getString(5));
+            ven.setFecha(tabla.getDate(6));
+            ven.setCedula_cliente(tabla.getInt(7));
+            ven.setNombre_cliente(tabla.getString(8));
+            ven.setTelefono_cliente(tabla.getInt(9));
+            
+            fachada.closeConection();
+        }
+        catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "         Ha ocurrido un problema, \n consulta con la base de datos fallida", "AutosABC", JOptionPane.ERROR_MESSAGE);        }
+        catch (Exception ex){
+            JOptionPane.showMessageDialog(null, "Ha ocurrido un problema!!", "AutosABC", JOptionPane.ERROR_MESSAGE);
+        }
+        return ven;
     }
 }
