85f25699bb7510de2438348519efd3788a2ee422
''Steven'' <''Steven''@Steven>
2016-11-22 02:50:42 -0500

    Modificados los metodos set y get a un tipo de dato acorde

--- src/ACCESO_DATOS/entidades_y_relaciones/OrdenDeTrabajo.java
+++ src/ACCESO_DATOS/entidades_y_relaciones/OrdenDeTrabajo.java
@@ -7,51 +7,49 @@ package ACCESO_DATOS.entidades_y_relaciones;
  */
 public class OrdenDeTrabajo {
         int  id_orden ;
-	Usuario id_usuario;
-	Repuesto id_repuesto;
-	Vehiculo id_vehiculo ;
-        Sede id_sede ;
+	int id_usuario;
+	int id_repuesto;
+	int id_vehiculo ;
+        int id_sede ;
 	String descripcion ;
 
     public int getId_orden() {
         return id_orden;
     }
 
-    public Usuario getId_usuario() {
+    public int getId_usuario() {
         return id_usuario;
     }
 
-    public Repuesto getId_repuesto() {
+    public int getId_repuesto() {
         return id_repuesto;
     }
 
-    public Vehiculo getId_vehiculo() {
+    public int getId_vehiculo() {
         return id_vehiculo;
     }
 
-    public Sede getId_sede() {
+    public int getId_sede() {
         return id_sede;
     }
-
- 
 
     public void setId_orden(int id_orden) {
         this.id_orden = id_orden;
     }
 
-    public void setId_usuario(Usuario id_usuario) {
+    public void setId_usuario(int id_usuario) {
         this.id_usuario = id_usuario;
     }
 
-    public void setId_repuesto(Repuesto id_repuesto) {
+    public void setId_repuesto(int id_repuesto) {
         this.id_repuesto = id_repuesto;
     }
 
-    public void setId_vehiculo(Vehiculo id_vehiculo) {
+    public void setId_vehiculo(int id_vehiculo) {
         this.id_vehiculo = id_vehiculo;
     }
 
-    public void setId_sede(Sede id_sede) {
+    public void setId_sede(int id_sede) {
         this.id_sede = id_sede;
     }
 
