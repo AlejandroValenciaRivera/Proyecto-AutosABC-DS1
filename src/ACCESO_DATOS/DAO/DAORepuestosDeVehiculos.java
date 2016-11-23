9eeed9fcbbf41631cda4f3a00b657fa7059238d3
Eriklp <lopez.erik@correounivalle.edu.co>
2016-11-22 22:53:39 -0500

    consultas de RepuestosDeVehiculos implementado

--- src/ACCESO_DATOS/DAO/DAORepuestosDeVehiculos.java
+++ src/ACCESO_DATOS/DAO/DAORepuestosDeVehiculos.java
@@ -8,6 +8,8 @@ package ACCESO_DATOS.DAO;
 import ACCESO_DATOS.conexion.*;
 import ACCESO_DATOS.entidades_y_relaciones.*;
 import java.sql.*;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
 /**
  *
  * @author Erik Lopez Pacheco
@@ -19,6 +21,12 @@ public class DAORepuestosDeVehiculos {
         fachada = new FachadaBD();
     }
     
+    /**
+     * 
+     * @param vinculacion
+     * @return int
+     * @proposito guardar en la base de datos el registor de una vinculacion que recive como parametro
+     */
     public int guardarVinculacion(RepuestosDeVehiculos vinculacion){
         String statement_save = "INSERT INTO repuestos_vehiculos VALUES ("
                 +vinculacion.getId_repuesto() +", "
@@ -39,7 +47,59 @@ public class DAORepuestosDeVehiculos {
         return numFilas;
     }
     
-    public RepuestosDeVehiculos consultarVinculacion(){
-        return null;
+    /**
+     * 
+     * @param id_vehiculo
+     * @return ArrayList<RespuestosDeVechiculos> *
+     * @proposito dado un id de vehiculo retorna un array de objeto RespuestosDeVehoculos asociados a ese id de vehiculo
+     */
+    public ArrayList<RepuestosDeVehiculos> consultarVinculacionVehiculo(int id_vehiculo){
+        ArrayList<RepuestosDeVehiculos> listaVinculacion = new ArrayList<>();
+        String sql_select = "SELECT * FROM repuestos_vehiculos WHERE id_vehiculo = " + id_vehiculo;
+        try{
+            Connection conn = fachada.getConnetion();
+            Statement sentencia = conn.createStatement();
+            ResultSet tabla = sentencia.executeQuery(sql_select);
+            
+            while(tabla.next()){
+                RepuestosDeVehiculos repVehi = new RepuestosDeVehiculos();
+                 repVehi.setId_repuesto(tabla.getInt(1));
+                 repVehi.setId_vehiculo(tabla.getInt(2));
+                 
+                 listaVinculacion.add(repVehi);
+                
+            }
+        }
+        catch(SQLException e){ 
+             JOptionPane.showMessageDialog(null, "         Ha ocurrido un problema, \n consulta con la base de datos fallida", "AutosABC", JOptionPane.ERROR_MESSAGE);         }
+         catch(Exception e){ 
+             JOptionPane.showMessageDialog(null, " Conexion con la base de datos fallida. \n Contacte inmediatamente con soporte");
+         }
+        return listaVinculacion;
+    }
+    
+    public ArrayList<RepuestosDeVehiculos> consultarVinculacionRepuesto(int id_repuesto){
+        ArrayList<RepuestosDeVehiculos> listaVinculacion = new ArrayList<>();
+        String sql_select = "SELECT * FROM repuestos_vehiculos WHERE id_repuesto = " + id_repuesto;
+        try{
+            Connection conn = fachada.getConnetion();
+            Statement sentencia = conn.createStatement();
+            ResultSet tabla = sentencia.executeQuery(sql_select);
+            
+            while(tabla.next()){
+                RepuestosDeVehiculos repVehi = new RepuestosDeVehiculos();
+                 repVehi.setId_repuesto(tabla.getInt(1));
+                 repVehi.setId_vehiculo(tabla.getInt(2));
+                 
+                 listaVinculacion.add(repVehi);
+                
+            }
+        }
+        catch(SQLException e){ 
+             JOptionPane.showMessageDialog(null, "         Ha ocurrido un problema, \n consulta con la base de datos fallida", "AutosABC", JOptionPane.ERROR_MESSAGE);         }
+         catch(Exception e){ 
+             JOptionPane.showMessageDialog(null, " Conexion con la base de datos fallida. \n Contacte inmediatamente con soporte");
+         }
+        return listaVinculacion;
     }
 }
--- src/ACCESO_DATOS/entidades_y_relaciones/RepuestosDeVehiculos.java
+++ src/ACCESO_DATOS/entidades_y_relaciones/RepuestosDeVehiculos.java
@@ -7,22 +7,22 @@ package ACCESO_DATOS.entidades_y_relaciones;
  */
 public class RepuestosDeVehiculos {
     
-        Repuesto id_repuesto ;
-	Vehiculo id_vehiculo ;
+        int id_repuesto ;
+	int id_vehiculo ;
 
-    public Repuesto getId_repuesto() {
+    public int getId_repuesto() {
         return id_repuesto;
     }
 
-    public Vehiculo getId_vehiculo() {
+    public int getId_vehiculo() {
         return id_vehiculo;
     }
 
-    public void setId_repuesto(Repuesto id_repuesto) {
+    public void setId_repuesto(int id_repuesto) {
         this.id_repuesto = id_repuesto;
     }
 
-    public void setId_vehiculo(Vehiculo id_vehiculo) {
+    public void setId_vehiculo(int id_vehiculo) {
         this.id_vehiculo = id_vehiculo;
     }
 
